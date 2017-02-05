f717832ce523b2c233a0fb6a379db5465f0bcc8a
max <max.morsky@gmail.com>
2017-02-05 15:53:10 +0200

    koneeseen vaihto

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,9 +1,13 @@
 class Beer < ActiveRecord::Base
-  include RatingAverage
+  # include RatingAverage
 
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
+  def average
+    binding.pry
+    ratings.map { |r| r.score }.sum / ratings.count.to_f
+  end
 
   def to_s
     "#{self.name} #{self.brewery.name} "
